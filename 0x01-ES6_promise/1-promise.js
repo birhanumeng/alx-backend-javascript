ee1c7b229d7470508aaab2f16240c5859371118f
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-19 08:33:38 +0000

    return a promise object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if(success){
+    if(success === true){
       resolve({
         status: 200,
 	body: "Success",
