3c320e2802363d4b028bbdd4e1ca3f5329f0750b
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-19 16:49:00 +0000

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,30 +2,30 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const promise1 = {
+  const user_promise = {
     status: 'pending ',
   };
-  const promise2 = {
+  const photo_promise = {
     status: 'pending ',
   };
 
   try {
     const signup = await signUpUser(firstName, lastName);
-    promise1.status = 'fulfilled';
-    promise1.value = signup;
+    user_promise.status = 'fulfilled';
+    user_promise.value = signup;
   } catch (err) {
-    promise1.status = 'rejected';
-    promise1.value = err.toString();
+    user_promise.status = 'rejected';
+    user_promise.value = err.toString();
   }
 
   try {
     const upload = await uploadPhoto(fileName);
-    promise2.status = 'fulfilled';
-    promise2.value = upload;
+    photo_promise.status = 'fulfilled';
+    photo_promise.value = upload;
   } catch (err) {
-    promise2.status = 'rejected';
-    promise2.value = err.toString();
+    photo_promise.status = 'rejected';
+    photo_promise = err.toString();
   }
 
-  return [promise1, promise2];
+  return [user_promise, photo_promise];
 }
