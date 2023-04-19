f534184d7d3fe792f14edca2c6a5c04be1ff7ffb
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-19 19:41:16 +0000

    check upload status

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { uploadPhoto, createUser } from './utils';
+
+async function asyncUploadUser() {
+  let photo;
+  let user;
+
+  try {
+    photo = await uploadPhoto();
+    user = await createUser();
+  } catch (error) {
+    photo = null;
+    user = null;
+  }
+  const result = {
+    photo,
+    user,
+  };
+
+  return result;
+}
