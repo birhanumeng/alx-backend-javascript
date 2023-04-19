6ed704df657219ab7e5c731be5d0394e7c31cc02
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-19 16:43:49 +0000

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,29 +2,30 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const user_promise = {
-    status: 'pending',
-    value: '',
+  const promise1 = {
+    status: 'pending ',
   };
-  const photo_promise = {
-    status: 'pending',
-    value: '',
+  const promise2 = {
+    status: 'pending ',
   };
+
   try {
-    const signupUser = await signUpUser(firstName, lastName);
-    user_promise.status = 'fulfilled';
-    user_promise.value = signupUser;
-  } catch (error) {
-    user_promise.status = 'rejected';
-    user_promise.value = error.toString();
+    const signup = await signUpUser(firstName, lastName);
+    promise1.status = 'fulfilled';
+    promise1.value = signup;
+  } catch (err) {
+    promise1.status = 'rejected';
+    promise1.value = err.toString();
   }
+
   try {
-    const uploadPhoto = await uploadPhoto(fileName);
-    photo_promise.status = 'fulfilled';
-    photo_promise.value = uploadPhoto;
-  } catch (error) {
-    photo_promise.status = 'rejected';
-    photo_promise.value = error.toString();
+    const upload = await uploadPhoto(fileName);
+    promise2.status = 'fulfilled';
+    promise2.value = upload;
+  } catch (err) {
+    promise2.status = 'rejected';
+    promise2.value = err.toString();
   }
-  return [user_promise, photo_promise];
+
+  return [promise1, promise2];
 }
