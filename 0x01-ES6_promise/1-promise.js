ffad1b0e016f5320705bc4949fe4bf9de64eaceb
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-19 08:44:25 +0000

    return a promise object

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if(success === true) {
       resolve({
-        status: 200,
-	body: 'Success',
+        "status": 200,
+	"body": 'Success',
       });
     } else {
       reject(new Error('The fake API is not working currently'));
