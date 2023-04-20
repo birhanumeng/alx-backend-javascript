97e764b1c832a0e95aae9a3e3666cdab7fcb9782
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-20 12:14:47 +0000

    make classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  numbers = [19, 20, 34];
-  res = [];
-  for(const num of numbers) {
-    res.push('ClassRoom {_maxStudentsSize: `${num}`}');
-  }
-
-  return res;
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
