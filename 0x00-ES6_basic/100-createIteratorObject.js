3ce9bb719d892033121ccba920c8b8f2cfe7de3d
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-21 17:40:35 +0000

    create iterator object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,21 @@
 export default function createIteratorObject(report) {
-  let iterable = [];
+  const iterator = {
+    [Symbol.iterator]() {
+      return {
+        next() {
+          for (const value of Object.values(report.allEmployees)) {
+            for (const emp of value) {
+              if(emp) {
+                return { done: false, value: emp };
+              } else {
+                return { done: true, value: undefined };
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
-	if (!report.allEmployees || typeof report.allEmployees !== 'object') {
-		return iterable;
-	}
-
-	iterable = {
-		* [Symbol.iterator]() {
-			for (const value of Object.values(report.allEmployees)) {
-				for (const i of value) {
-					yield i;
-				}
-			}
-		},
-	};
-
-	return iterable;
+  return iterator;
 }
