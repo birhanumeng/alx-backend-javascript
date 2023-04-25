74ab50753015b119fa06f93bd42ffee0735bb8cf
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 13:56:01 +0000

    produce typped array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8view = new Int8Array(buffer, 0);
+  const int8view = new Int8Array(buffer);
 
   if (position > length - 1) {
     throw Error('Position outside range');
