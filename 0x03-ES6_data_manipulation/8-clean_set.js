bc1e0582d5ce6ac70156e343f40872b99017cb4c
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 15:37:35 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,11 @@
 export default function cleanSet(set, startString) {
   const string = [];
 
-  if (!startString.length || !startString) {
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
     return '';
   }
 
