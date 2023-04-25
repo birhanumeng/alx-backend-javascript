c6b327ded68c16f3559e859d1b684783aa4c0d37
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 07:06:20 +0000

    student id lists

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
-export default getListStudentIds(students) {
+export default function getListStudentIds(students) {
   if(!Array.isArray(students)) {
     return [];
   }
 
-  const ids = students.map((student) => student.id);
+  const ids = students.map(student => student.id);
 
   return ids;
 }
