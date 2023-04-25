f516589d1b289f00d2446e0a4648bf2577ada52d
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 06:32:56 +0000

    student lists

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,9 @@
 export default function getListStudents() {
-  const arr = [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
-    { id: 2, firstName: "James", location: "Columbia" },
-    { id: 5, firstName: "Serena", location: "San Francisco" }
+  const obj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
-  return arr;
+  return obj;
 }
