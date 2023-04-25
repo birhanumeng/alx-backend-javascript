092d817e9afb8622672111e465b872eeb1881440
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 08:29:18 +0000

    sum student id

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,3 @@
 export default function getStudentIdsSum(students) {
-  const sumId = 0;
-  return students.reduce((student) => sumId + student.id);
+  return students.reduce((student) => sum(student.id));
 }
