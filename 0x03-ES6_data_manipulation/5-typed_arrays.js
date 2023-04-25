59589918a6464c00f56d8f5d0a2edd647619907f
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 13:53:00 +0000

    produce typped array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,12 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
+  const int8view = new Int8Array(buffer, 0);
 
-  return buffer;
+  if (position > length - 1) {
+    throw Error('Position outside range');
+  }
+
+  int8view.setInt8(position, value);
+
+  return int8view;
 }
