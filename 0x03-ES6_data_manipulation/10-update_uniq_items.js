540ca7948ecd518498d5b6ddf9c56809a10e5c03
birhanumeng <eyerusalemmengiste@gmail.com>
2023-04-25 16:18:11 +0000

    update map

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -3,7 +3,7 @@ export default function updateUniqueItems(map) {
     throw Error('Cannot process');
   }
 
-  for (const [key, value] of map.item()) {
+  for (const [key, value] of map) {
     if (value === 1) {
       map.set(key, 100);
     }
