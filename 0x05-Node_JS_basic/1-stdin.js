e848f15bec972c6d5bd4e05e65e0105229e7dd16
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-13 08:20:32 +0000

    print message using process

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+let message = 'Welcome to Holberton School, what is your name?\n';
+
+process.stdout.write(message);
+
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+  if (data !== null) {
+    process.stdout.write('Your name is:' + data.toString().trim());
+  }
+});
+
+process.on('exit', () => {
+  process.stdout.write('\nThis important software is now closing\n');
+});
