1b3807849ea7f1d38d56ed7441920d4b3d650a94
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-15 18:43:07 +0000

    javascript unit test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,14 +3,9 @@ const mocha = require('mocha');
 
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('should return sum of integers', () => {
-    assert.strictEqual(calculateNumber(1, 1), 2);
-    assert.strictEqual(calculateNumber(1, -1), 0);
-    assert.strictEqual(calculateNumber(-1, -1), -2);
-  });
-
-  it('should round floats', () => {
+describe('calculattion', () => {
+  it('should return sum of integers or round to the above for float', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
