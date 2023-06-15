aae2767f8e60a6dd1628f03e41c0f0c97644ab38
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-15 18:52:16 +0000

    javascript unit test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,4 +1,4 @@
-module.exports = function calculateNumber(a, b = 0) {
+function calculateNumber(a, b = 0) {
   const num_a = Number(a);
   const num_b = Number(b);
   if (Number.isNaN(num_a) || Number.isNaN(num_b))
@@ -6,3 +6,5 @@ module.exports = function calculateNumber(a, b = 0) {
   
     return Math.round(num_a) + Math.round(num_b);
   };
+
+module.export = calculateNumber;
