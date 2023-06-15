2328e27c6ad12f25103db13151695c06cca24a1a
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-15 18:46:52 +0000

    javascript unit test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,9 +1,8 @@
 module.exports = function calculateNumber(a, b = 0) {
-  const aNum = Number(a);
-  const bNum = Number(b);
-  
-  if (Number.isNaN(aNum) || Number.isNaN(bNum))
+  const a = Number(a);
+  const b = Number(b);
+  if (Number.isNaN(a) || Number.isNaN(b))
     throw TypeError('Parameters must be numbers');
   
-    return Math.round(aNum) + Math.round(bNum);
+    return Math.round(a) + Math.round(b);
   };
