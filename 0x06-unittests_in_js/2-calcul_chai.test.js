fb53787b669d725633f04a0d2c5f3368fe8f7b4e
birhanumeng <eyerusalemmengiste@gmail.com>
2023-06-16 01:53:14 +0000

    test using chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,27 +1,15 @@
 var chai = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', function () {
-  describe('SUM no Round', function () {
-    it('should return 5', function () {
+describe('calculation', function () {
+  describe('SUM', function () {
+    it('return 5', function () {
       chai.expect(calculateNumber('SUM', 1, 4)).to.equal(5);
     });
-  });
-
-  describe('SUM first round', function () {
-    it('should return 6', function () {
+    it('return 6', function () {
       chai.expect(calculateNumber('SUM', 2.4, 4)).to.equal(6);
     });
-  });
-
-  describe('SUM second round ', function () {
-    it('should return 6', function () {
-      chai.expect(calculateNumber('SUM', 4, 2.4)).to.equal(6);
-    });
-  });
-
-  describe('SUM both round', function () {
-    it('should return 6', function () {
+    it('return 6', function () {
       chai.expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
     });
   });
